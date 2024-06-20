41238e5750b2816f2cca38e30259a6348fa20ebe
jefriwahyu <92837751+jefriwahyu@users.noreply.github.com>
2024-06-20 21:06:26 +0700

    menambahkan data buku pada js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,58 @@
-function addNewBook(newbookObject) {
-    const textTitle = document.createElement('h2');
-    textTitle.innerText = todoObject.task;
-   
-    const textTimestamp = document.createElement('p');
-    textTimestamp.innerText = todoObject.timestamp;
-   
-    const textContainer = document.createElement('div');
-    textContainer.classList.add('inner');
-    textContainer.append(textTitle, textTimestamp);
-   
-    const container = document.createElement('div');
-    container.classList.add('item', 'shadow');
-    container.append(textContainer);
-    container.setAttribute('id', `todo-${todoObject.id}`);
-   
-    return container;
+const books = [];
+let isComplete = false;
+const RENDER_EVENT = 'render-book';
+
+document.addEventListener('DOMContentLoaded', function () {
+  const submitForm = document.getElementById('form');
+  submitForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    addBook();
+  })
+})
+
+function addBook() {
+  const titleBook = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const year = document.getElementById('year').value;
+
+  const id = generatedID();
+  const isComplete = isCompleted();
+  const bookObject = generateBookObject(id, titleBook, author, year, isComplete);
+  books.push(bookObject);
+
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const checkbox = document.getElementById('filter-mobile-color-1');
+
+  checkbox.addEventListener('change', function() {
+    if (this.checked) {
+      isComplete = true;
+    } else {
+      isComplete = false;
+    }
+  });
+});
+
+function isCompleted() {
+  return isComplete;
+}
+
+function generatedID() {
+  return +new Date();
+}
+
+function generateBookObject(id, title, author, year, isComplete) {
+  return {
+    id,
+    title,
+    author,
+    year,
+    isComplete
   }
+}
+
+document.addEventListener(RENDER_EVENT, function () {
+  console.log(books);
+})
