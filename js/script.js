4483a6edcdd528f64027186f5c49a315faf543a2
jefriwahyu <92837751+jefriwahyu@users.noreply.github.com>
2024-06-20 23:16:26 +0700

    menambahkan dom unfinished read

--- js/script.js
+++ js/script.js
@@ -56,3 +56,35 @@ function generateBookObject(id, title, author, year, isComplete) {
 document.addEventListener(RENDER_EVENT, function () {
   console.log(books);
 })
+
+function addBooks(bookObject) {
+  const title = document.createElement('h2');
+  title.classList.add('text-xl', 'font-bold', 'mb-2');
+  title.innerText = bookObject.title;
+
+  const author = document.createElement('p');
+  author.classList.add('text-gray-700');
+  author.innerText = bookObject.author;
+
+  const year = document.createElement('p');
+  author.classList.add('text-gray-700');
+  year.innerText = bookObject.year;
+
+  const container = document.createElement('div');
+  container.classList.add('bg-white', 'p-6', 'rounded-lg', 'shadow-lg');
+  container.append(title, author, year);
+  container.setAttribute('id', `book-${bookObject.id}`);
+
+  return container;
+
+}
+
+document.addEventListener(RENDER_EVENT, function () {
+  const uncompletedBookList = document.getElementById('not-finished');
+  uncompletedBookList.innerHTML = '';
+
+  for (const bookItem of books) {
+    const bookElement = addBooks(bookItem);
+    uncompletedBookList.append(bookElement);
+  }
+});
